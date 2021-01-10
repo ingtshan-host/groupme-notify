734c639ff6493eaefb43ff543d3f8d4c76fab685
Nicolas <nhomble@terpmail.umd.edu>
2021-01-10 15:55:22 -0500

    adds ability to send with bot api

--- main.go
+++ main.go
@@ -10,24 +10,34 @@ import (
 
 var (
 	groupId = flag.String("groupId", "", "id of the group to post the message into")
+	botId   = flag.String("botId", "", "id of the bot post the message")
 	message = flag.String("m", "", "the message")
 )
 
 func main() {
 	flag.Parse()
-	if len(*groupId) <= 0 {
+	if len(*groupId) <= 0 && len(*botId) <= 0 {
 		flag.Usage()
-		panic("groupId is empty")
+		panic("groupId and botId cannot both be empty")
 	}
 	rand.Seed(time.Now().UnixNano())
 	id := fmt.Sprintf("%d%d", rand.Int63(), rand.Int63())
 	fmt.Printf("messageId=%s\n", id)
 	provider := groupme.EnvironmentTokenProvider{}
 	client, _ := groupme.NewClient(provider)
-	_, err := client.Messages.Send(*groupId, &groupme.SendMessageCommand{
-		SourceGuid: id,
-		Text:       *message,
-	})
+
+	var err error
+	if len(*botId) > 0 {
+		err = client.Bots.Send(groupme.BotMessageCommand{
+			BotID:   *botId,
+			Message: *message,
+		})
+	} else {
+		_, err = client.Messages.Send(*groupId, &groupme.SendMessageCommand{
+			SourceGuid: id,
+			Text:       *message,
+		})
+	}
 	must(err)
 }
 
